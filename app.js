92dbc595538ad5204b1edcda8370100252b057a3
amanyaboelyazeed <45687614+amanyaboelyazeed@users.noreply.github.com>
2018-12-12 09:19:14 +0200

    Refactor with await/async

--- app.js
+++ app.js
@@ -1,85 +1,66 @@
 //const express = require('express');
-const nodemailer = require('nodemailer');
-const smtpTransport = require('nodemailer-smtp-transport');
-const amqp = require('amqplib/callback_api');
-const config = require('./config.json');
+const nodemailer = require('nodemailer')
+const smtpTransport = require('nodemailer-smtp-transport')
+const amqp = require('amqplib')
+const config = require('./config.json')
 
-
-amqp.connect(config.rabbitmq.url, function(err, conn) {
-  if (closeOnErr(conn, err)) return;
-  conn.createChannel(function(err, ch) {    
-    ch.assertQueue(config.rabbitmq.queue, {durable: false});
-    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", config.rabbitmq.queue);
-    if (closeOnErr(conn, err)) return;
-    ch.consume(config.rabbitmq.queue, function(msg) {
-        if (msg.content) {            
-            processMessage(msg, function(error){
-                try {
-                    if (error){                                                                            
-                        ch.nack(msg, false, false); 
-                    }
-                    else {                    
-                        ch.ack(msg);
-                    }
-                }
-                catch (e){                   
-                    closeOnErr(conn, e);
-                }
-            })                      
+async function startProcessing() {       
+    const conn = await amqp.connect(config.rabbitmq.url)
+    const channel = await conn.createChannel()
+    await channel.consume(config.rabbitmq.queue, async message => {
+        if (message.content) {
+            const processed = await processMessage(message).catch(err => console.log('Process Message Error: ', err))      
+            const status = await acknowledgeMessage(channel, message, processed).catch(err => console.log('Acknowledge Message Error: ', err))
+            console.log(status)
         }
-    }, {noAck: false});
-  });
-});
-
-
-function processMessage(msg, cb) {
-    try{
-        console.log("Received Message : %s", msg.content);
-        var obj=JSON.parse(msg.content.toString())
-    }
-    catch (err){
-        console.log("Invalid Message Format!");  
-        cb(err);
-        return;
-    }  
- 
-    var mailOptions = {
-        from: config.mail.user,
-        to: obj.recipient,
-        subject: obj.subject,
-        text: obj.body
-      };
-
-    var transporter = nodemailer.createTransport(smtpTransport({
-        service: config.mail.service,
-        host: config.mail.host,
-        auth: {
-          user: config.mail.user,
-          pass: config.mail.password
-        }
-    })); 
+    }, { noAck: false })
    
-    transporter.sendMail(mailOptions, function(error, info){
-        console.log("Send Mail to: %s", mailOptions.to);
-        if (error) {
-            console.log(error);
-            cb(true);
-        } else {
-            console.log('Email sent: ' + info.response);
-            cb(false);                     
-        }
-    });
 }
 
-function closeOnErr(amqpConn, err) {
-    if (!err) return false;
-    console.error("[AMQP] error", err);
-    amqpConn.close();
-    return true;
-  }
-
-
-
-
+function processMessage(queueMsg) {
+    return new Promise((resolve, reject) => {
+        var msg=JSON.parse(queueMsg.content.toString())
   
- 
+        var mailOptions = {
+            from: config.mail.user,
+            to: msg.recipient,
+            subject: msg.subject,
+            text: msg.body
+        }
+        var transporter = nodemailer.createTransport(smtpTransport({
+            service: config.mail.service,
+            host: config.mail.host,
+            auth: {
+              user: config.mail.user,
+              pass: config.mail.password
+            }
+        })) 
+       
+        transporter.sendMail(mailOptions, function(error, info){
+            console.log("Send Mail to: %s", mailOptions.to)
+            if (error) {
+                console.log(error)
+                reject(error)
+            } else {
+                console.log('Email sent: ' + info.response)
+                resolve(true)               
+            }
+        })
+    })   
+}
+
+function acknowledgeMessage(ch, msg, processed) {
+    return new Promise((resolve) => {
+        if (processed){                                                                            
+            ch.ack(msg) 
+            console.log('Message ACK..')       
+        }
+        else {
+            ch.nack(msg, false, false)   
+            console.log('Message NACK..')                                
+        } 
+        resolve('Message Processed!')   
+    })
+}
+
+startProcessing()
